66415fb0d4fc848f0965f7b57e2def3de5652cc5
Ansh Rathod <anshrathod29@gmail.com>
2025-06-10 16:02:39 +0530

    Update imgpim to version 0.1.4 with new URL and SHA256 checksum

--- Formula/imgpim.rb
+++ Formula/imgpim.rb
@@ -1,8 +1,8 @@
 class Imgpim < Formula
   desc "CLI tool for lossless image compression using open-source libraries"
   homepage "https://github.com/Ansh-Rathod/imgpim"
-  url "https://github.com/Ansh-Rathod/imgpim/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "f31c0c17d76cfbdec1b69ea33b090b74f17d9f87168d9ac0c3755723111a2bc8"
+  url "https://github.com/Ansh-Rathod/imgpim/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "140e61ece574bd31c1d15eb09dc1c29cc4ac94bc22311b8de8f21d8a04f55d49"
   license "MIT"
 
   depends_on "go" => :build
